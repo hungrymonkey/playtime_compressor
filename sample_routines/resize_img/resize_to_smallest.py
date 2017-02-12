7d94dc50ccb3473f4bb32392883a433933ede8d1
moo <boringmonkey@gmail.com>
2017-02-11 20:39:04 -0800

    simple script to resize image to the smaller image sample

--- sample_routines/resize_img/resize_to_smallest.py
+++ sample_routines/resize_img/resize_to_smallest.py
@@ -2,13 +2,20 @@
 
 import numpy as np
 import scipy as sp
+import scipy.misc
 
 img1_filename = '250px-Fox_Sports_logo.png'
 img2_filename = 'FOX_Sports_logo2.png'
 def main():
     img1 = sp.misc.imread( img1_filename )
     img2 = sp.misc.imread( img2_filename )
-
+    if img1.size > img2.size:
+        outimg = sp.misc.imresize( img1, img2.shape)
+    else:
+        outimg = sp.misc.imresize( img2, img1.shape)
+    sp.misc.imsave('smaller_img.png', outimg)
+        
+    
     
 if __name__ == "__main__":
     main()
