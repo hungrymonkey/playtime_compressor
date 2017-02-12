9deaaa332334525b3f58cdf2332b2d3948f22abf
moo <boringmonkey@gmail.com>
2017-02-11 20:23:23 -0800

    commiting terrible code

---
+++ sample_routines/resize_img/resize_to_smallest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import scipy as sp
+
+img1_filename = '250px-Fox_Sports_logo.png'
+img2_filename = 'FOX_Sports_logo2.png'
+def main():
+    img1 = sp.misc.imread( img1_filename )
+    img2 = sp.misc.imread( img2_filename )
+
+    
+if __name__ == "__main__":
+    main()
